f4844dc6f0fbec9ffaf3206e4e35d8524023b7c2
JuanFerGiraldo <juan.giraldo11@gmail.com>
2026-04-20 13:32:00 -0500

    week1: base framework setup – pytest + playwright, tests procedurales (2 passed)

---
+++ week1-framework/tests/test_procedural.py
@@ -0,0 +1,24 @@
+import pytest
+from playwright.sync_api import sync_playwright, expect
+
+def test_login_procedural():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://the-internet.herokuapp.com/login")
+        page.fill("#username", "tomsmith")
+        page.fill("#password", "SuperSecretPassword!")
+        page.click("button[type='submit']")
+        expect(page.locator("#flash")).to_contain_text("You logged into a secure area!")
+        browser.close()
+
+def test_invalid_login_procedural():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://the-internet.herokuapp.com/login")
+        page.fill("#username", "tomsmith")
+        page.fill("#password", "wrongpass")
+        page.click("button[type='submit']")
+        expect(page.locator("#flash")).to_contain_text("Your password is invalid")
+        browser.close()
